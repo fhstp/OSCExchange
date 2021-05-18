55885944d03ca3bee614e5f2892659816ac743ae
KingVanti <kingvanti650@gmail.com>
2021-05-18 12:51:58 +0200

    Add type for OSCAddresses

---
+++ oscexchange/src/main/java/ac/at/fhstp/digitech/oscexchange/OSCAddress.java
@@ -0,0 +1,28 @@
+package ac.at.fhstp.digitech.oscexchange;
+
+import java.util.Optional;
+import java.util.regex.Pattern;
+
+public class OSCAddress {
+
+    public static Pattern pattern = Pattern.compile("^(/[a-zA-Z]+)+$");
+
+
+    public static boolean isValidOSCAddress(String value) {
+        return value != null && pattern.matcher(value).find();
+    }
+
+    public static Optional<OSCAddress> create(String value) {
+        if (isValidOSCAddress(value)) return Optional.of(new OSCAddress(value));
+        else return Optional.empty();
+    }
+
+
+    public final String value;
+
+
+    public OSCAddress(String address) {
+        this.value = address;
+    }
+
+}
