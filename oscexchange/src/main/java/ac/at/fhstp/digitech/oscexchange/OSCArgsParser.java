4b64919bbd84ff13154e498fa96b6ae334e3e4a9
KingVanti <kingvanti650@gmail.com>
2021-05-19 12:30:28 +0200

    Document OSCArgsParser

--- oscexchange/src/main/java/ac/at/fhstp/digitech/oscexchange/OSCArgsParser.java
+++ oscexchange/src/main/java/ac/at/fhstp/digitech/oscexchange/OSCArgsParser.java
@@ -2,12 +2,29 @@ package ac.at.fhstp.digitech.oscexchange;
 
 import java.util.Optional;
 
+/**
+ * Parses OSCArgs to some type
+ *
+ * @param <T>The type to parse to
+ */
 @FunctionalInterface
 public interface OSCArgsParser<T> {
 
+    /**
+     * Attempts to parse the given OSCArgs to the parsers type
+     *
+     * @param args The OSCArgs that should be parsed
+     * @return Optional parsed object. Empty if parsing was not successful
+     */
     Optional<T> parse(OSCArgs args);
-
-
+    
+    /**
+     * Attempts to parse the given OSCArgs to the parsers type
+     *
+     * @param args The OSCArgs that should be parsed
+     * @return The parsed object
+     * @throws OSCParseException Thrown if parsing was not successful
+     */
     default T forceParse(OSCArgs args) throws OSCParseException {
         return parse(args).orElseThrow(() -> new OSCParseException(args));
     }
