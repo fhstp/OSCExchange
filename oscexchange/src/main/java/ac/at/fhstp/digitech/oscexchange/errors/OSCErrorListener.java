163733a1b1425991aafecdf43a8d95621a1143d6
KingVanti <kingvanti650@gmail.com>
2021-05-19 12:21:34 +0200

    Document error listener

--- oscexchange/src/main/java/ac/at/fhstp/digitech/oscexchange/errors/OSCErrorListener.java
+++ oscexchange/src/main/java/ac/at/fhstp/digitech/oscexchange/errors/OSCErrorListener.java
@@ -1,8 +1,16 @@
 package ac.at.fhstp.digitech.oscexchange.errors;
 
+/**
+ * A listener for when an error occurs
+ */
 @FunctionalInterface
 public interface OSCErrorListener {
 
+    /**
+     * Handles the occurred error
+     *
+     * @param error The error that occurred
+     */
     void handle(OSCError error);
 
 }
