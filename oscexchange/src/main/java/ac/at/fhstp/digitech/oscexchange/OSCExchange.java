5f8e25c82e2e34177004bfefbfdf5e38f0e89ba0
KingVanti <kingvanti650@gmail.com>
2021-05-19 12:46:37 +0200

    Document OSCExchange

--- oscexchange/src/main/java/ac/at/fhstp/digitech/oscexchange/OSCExchange.java
+++ oscexchange/src/main/java/ac/at/fhstp/digitech/oscexchange/OSCExchange.java
@@ -6,16 +6,36 @@ import java.util.ArrayList;
 
 import ac.at.fhstp.digitech.oscexchange.errors.OSCErrorListener;
 
+/**
+ * Stores all information about an OSC exchange between two devices
+ */
 public final class OSCExchange {
 
+    /**
+     * Instantiates a new builder with which a new OSCExchange can be constructed
+     *
+     * @return The new builder
+     */
     public static Builder buildNew() {
         return new Builder();
     }
 
+    /**
+     * Runs the given exchange between the given devices
+     *
+     * @param exchange   The exchange to run
+     * @param devicePair The devices between which to run the exchange
+     */
     public static void runBetween(OSCExchange exchange, OSCDevicePair devicePair) {
         LiveOSCExchange.start(exchange, devicePair);
     }
 
+    /**
+     * Runs the given exchange asynchronously between the given devices
+     *
+     * @param exchange   The exchange to run
+     * @param devicePair The devices between which to run the exchange
+     */
     public static void runBetweenAsync(OSCExchange exchange, OSCDevicePair devicePair) {
         AsyncTask.execute(() -> runBetween(exchange, devicePair));
     }
@@ -41,6 +61,9 @@ public final class OSCExchange {
     }
 
 
+    /**
+     * Used to build instances of OSCExchange
+     */
     public static class Builder {
 
         private final ArrayList<OSCRequest> requests;
@@ -53,38 +76,95 @@ public final class OSCExchange {
         }
 
 
+        /**
+         * Adds a send request to the request-chain
+         *
+         * @param address The address to send the request to
+         * @param args    The arguments to send to the address
+         * @return The same builder for method chaining
+         */
         public Builder send(OSCAddress address, OSCArgs args) {
             requests.add(new OSCSendRequest(address, args));
             return this;
         }
 
+        /**
+         * Adds a receive request to the request-chain
+         *
+         * @param address  The address on which the expected message will arrive
+         * @param listener A listener to be called when the message arrives. Null if you need no listener
+         * @return The same builder for method chaining
+         */
         public Builder receive(OSCAddress address, OSCRawReceiveListener listener) {
             return receive(address, null, listener);
         }
 
+        /**
+         * Adds a receive request to the request-chain
+         *
+         * @param address The address on which the expected message will arrive
+         * @return The same builder for method chaining
+         */
         public Builder receive(OSCAddress address) {
             return receive(address, (OSCArgsValidator) null, null);
         }
 
+        /**
+         * Adds a receive request to the request-chain
+         *
+         * @param address   The address on which the expected message will arrive
+         * @param validator A validator to be used on the received data. Null if you need no validation
+         * @param listener  A listener to be called when the message arrives. Null if you need no listener
+         * @return The same builder for method chaining
+         */
         public Builder receive(OSCAddress address, OSCArgsValidator validator, OSCRawReceiveListener listener) {
             requests.add(new OSCRawReceiveRequest(address, validator, listener));
             return this;
         }
 
+        /**
+         * Adds a receive request to the request-chain
+         *
+         * @param address  The address on which the expected message will arrive
+         * @param parser   A parser to be used on the received data
+         * @param listener A listener to be called when the message arrives. Null if you need no listener
+         * @return The same builder for method chaining
+         */
         public <T> Builder receive(OSCAddress address, OSCArgsParser<T> parser, OSCParsedReceiveListener<T> listener) {
             return receive(address, null, parser, listener);
         }
 
+        /**
+         * Adds a receive request to the request-chain
+         *
+         * @param address   The address on which the expected message will arrive
+         * @param validator A validator to be used on the received data. Null if you need no validation
+         * @param parser    A parser to be used on the received data
+         * @param listener  A listener to be called when the message arrives. Null if you need no listener
+         * @return The same builder for method chaining
+         */
         public <T> Builder receive(OSCAddress address, OSCArgsValidator validator, OSCArgsParser<T> parser, OSCParsedReceiveListener<T> listener) {
             requests.add(new OSCParsedReceiveRequest<>(address, validator, parser, listener));
             return this;
         }
 
+        /**
+         * Adds an error listener to the exchange
+         *
+         * @param errorListener The listener to be called when an error occurs. Null if you need no listener
+         * @return The same builder for method chaining
+         */
         public Builder onError(OSCErrorListener errorListener) {
             this.errorListener = errorListener;
             return this;
         }
 
+        /**
+         * Adds a completion listener to the exchange
+         *
+         * @param completeListener The listener to be called when the exchange completes without error. Null if you need no listener
+         * @return The same builder for method chaining
+         */
         public OSCExchange onComplete(OSCExchangeCompleteListener completeListener) {
             return new OSCExchange(requests.toArray(new OSCRequest[0]),
                                    errorListener,
