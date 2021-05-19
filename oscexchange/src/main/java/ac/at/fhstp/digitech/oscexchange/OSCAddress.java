56820a2c1d9fb31701b3c887d9b29f08273b1b7b
KingVanti <kingvanti650@gmail.com>
2021-05-19 12:25:33 +0200

    Document OSCAddress

--- oscexchange/src/main/java/ac/at/fhstp/digitech/oscexchange/OSCAddress.java
+++ oscexchange/src/main/java/ac/at/fhstp/digitech/oscexchange/OSCAddress.java
@@ -3,15 +3,35 @@ package ac.at.fhstp.digitech.oscexchange;
 import java.util.Optional;
 import java.util.regex.Pattern;
 
+/**
+ * Stores an OSC-address, such as "/my/osc/address"
+ *
+ * @see OSCAddress#create(String)
+ */
 public final class OSCAddress {
 
+    /**
+     * The pattern used to validate OSCAddresses
+     */
     public static Pattern pattern = Pattern.compile("^(/[a-zA-Z0-9]+)+$");
 
 
+    /**
+     * Checks if a string is a valid OSCAddress
+     *
+     * @param value The string to be checked
+     * @return True if the string is a valid OSCAddress
+     */
     public static boolean isValidOSCAddress(String value) {
         return value != null && pattern.matcher(value).find();
     }
 
+    /**
+     * Creates an OSCAddress
+     *
+     * @param value The string from which to create the address
+     * @return An optional address. Has a value if validation succeeded, empty otherwise
+     */
     public static Optional<OSCAddress> create(String value) {
         if (isValidOSCAddress(value)) return Optional.of(new OSCAddress(value));
         else return Optional.empty();
