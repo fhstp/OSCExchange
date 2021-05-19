54d9433b806d5d39fc1b354a81d83a5a5e792318
KingVanti <kingvanti650@gmail.com>
2021-05-19 12:47:45 +0200

    Document OSCParseException

--- oscexchange/src/main/java/ac/at/fhstp/digitech/oscexchange/OSCParseException.java
+++ oscexchange/src/main/java/ac/at/fhstp/digitech/oscexchange/OSCParseException.java
@@ -1,7 +1,13 @@
 package ac.at.fhstp.digitech.oscexchange;
 
+/**
+ * An exception that occurs when invalid OSCArgs are force-parsed
+ */
 public class OSCParseException extends Exception {
 
+    /**
+     * The args that were attempted to parse
+     */
     public final OSCArgs args;
 
     public OSCParseException(OSCArgs args) {
