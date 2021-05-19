d714a079951a4b9338261281735f6f1f0edced1a
KingVanti <kingvanti650@gmail.com>
2021-05-19 12:40:14 +0200

    Document OSCDevicePair

--- oscexchange/src/main/java/ac/at/fhstp/digitech/oscexchange/OSCDevicePair.java
+++ oscexchange/src/main/java/ac/at/fhstp/digitech/oscexchange/OSCDevicePair.java
@@ -2,9 +2,18 @@ package ac.at.fhstp.digitech.oscexchange;
 
 import java.net.InetSocketAddress;
 
+/**
+ * Contains information about a pair of devices that communicate over OSC
+ */
 public final class OSCDevicePair {
 
+    /**
+     * The address of this device
+     */
     public final InetSocketAddress local;
+    /**
+     * The address of the other device
+     */
     public final InetSocketAddress remote;
 
 
