bc21274848f643ae148f8e8c04f484cb060355f5
KingVanti <kingvanti650@gmail.com>
2021-05-19 10:42:03 +0200

    Refactor class to be final

--- oscexchange/src/main/java/ac/at/fhstp/digitech/oscexchange/OSCArgs.java
+++ oscexchange/src/main/java/ac/at/fhstp/digitech/oscexchange/OSCArgs.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-public class OSCArgs {
+public final class OSCArgs {
 
     public static OSCArgs single(Object arg) {
         return multiple(arg);
