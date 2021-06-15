972ed434041c4083f7bcd04e143a0d266b77e5f3
KingVanti <kingvanti650@gmail.com>
2021-06-15 10:14:16 +0200

    Add logic for opening ports

--- oscexchange/src/main/java/ac/at/fhstp/digitech/oscexchange/OSCDevicePair.kt
+++ oscexchange/src/main/java/ac/at/fhstp/digitech/oscexchange/OSCDevicePair.kt
@@ -1,5 +1,8 @@
 package ac.at.fhstp.digitech.oscexchange
 
+import ac.at.fhstp.digitech.oscexchange.errors.OSCPortOpeningError
+import com.illposed.osc.transport.udp.OSCPortIn
+import com.illposed.osc.transport.udp.OSCPortOut
 import java.net.InetSocketAddress
 
 /**
@@ -14,4 +17,26 @@ class OSCDevicePair @PublicApi constructor(
      * The address of the other device
      */
     @PublicApi val remote: InetSocketAddress
-)
+) {
+
+    fun tryOpenInPort() =
+        try {
+            Result.success(OSCPortIn(local))
+        } catch (e: Exception) {
+            val error = OSCPortOpeningError(
+                OSCPort.In, "Could not open in port!", e
+            )
+            Result.failure(error)
+        }
+
+    fun tryOpenOutPort() =
+        try {
+            Result.success(OSCPortOut(remote))
+        } catch (e: Exception) {
+            val error = OSCPortOpeningError(
+                OSCPort.Out, "Could not open out port!", e
+            )
+            Result.failure(error)
+        }
+
+}
