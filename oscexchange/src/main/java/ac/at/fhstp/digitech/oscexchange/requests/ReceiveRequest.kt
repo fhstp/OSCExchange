530e906ff087d48c7d19b2fc7071e294a693e5f5
KingVanti <kingvanti650@gmail.com>
2021-06-15 08:43:46 +0200

    Refactor request classes
    Make builder classes inner classes of request classes

--- oscexchange/src/main/java/ac/at/fhstp/digitech/oscexchange/requests/ReceiveRequest.kt
+++ oscexchange/src/main/java/ac/at/fhstp/digitech/oscexchange/requests/ReceiveRequest.kt
@@ -21,58 +21,58 @@ data class ReceiveRequest(
 
         @PublicApi
         fun new(address: OSCAddress) =
-            ReceiveRequestBuilder(
+            Builder(
                 address, noReceiveHandling, null,
                 noValidation, Request.noErrorHandling
             )
 
     }
 
-}
+    data class Builder(
+        private val address: OSCAddress,
+        private val onReceived: (OSCArgs) -> Unit,
+        private val parser: RequestParser<*>?,
+        private val validator: (OSCArgs) -> Boolean,
+        private val onError: (OSCError) -> Unit
+    ) : RequestBuilder<ReceiveRequest> {
 
-data class ReceiveRequestBuilder(
-    private val address: OSCAddress,
-    private val onReceived: (OSCArgs) -> Unit,
-    private val parser: RequestParser<*>?,
-    private val validator: (OSCArgs) -> Boolean,
-    private val onError: (OSCError) -> Unit
-) : RequestBuilder<ReceiveRequest> {
+        override fun build(): ReceiveRequest =
+            ReceiveRequest(address, onError, buildReceiveHandler())
 
-    override fun build(): ReceiveRequest =
-        ReceiveRequest(address, onError, buildReceiveHandler())
+        private fun buildReceiveHandler() =
+            { args: OSCArgs ->
+                if (validator(args)) {
+                    onReceived(args)
 
-    private fun buildReceiveHandler() =
-        { args: OSCArgs ->
-            if (validator(args)) {
-                onReceived(args)
+                    parser?.tryParse(args, onError)
+                } else
+                    onError(OSCValidationError(args))
+                Unit
+            }
 
-                parser?.tryParse(args, onError)
-            } else
-                onError(OSCValidationError(args))
-            Unit
-        }
+        @PublicApi
+        fun onReceive(onReceived: (OSCArgs) -> Unit) =
+            copy(onReceived = onReceived)
 
-    @PublicApi
-    fun onReceive(onReceived: (OSCArgs) -> Unit) =
-        copy(onReceived = onReceived)
+        @PublicApi
+        fun <T> withParser(
+            parser: (OSCArgs) -> T?,
+            onParseSuccess: (T) -> Unit
+        ) =
+            withParser(parser = RequestParser(parser, onParseSuccess))
 
-    @PublicApi
-    fun <T> withParser(
-        parser: (OSCArgs) -> T?,
-        onParseSuccess: (T) -> Unit
-    ) =
-        withParser(parser = RequestParser(parser, onParseSuccess))
+        @PublicApi
+        fun withParser(parser: RequestParser<*>) =
+            copy(parser = parser)
 
-    @PublicApi
-    fun withParser(parser: RequestParser<*>) =
-        copy(parser = parser)
+        @PublicApi
+        fun withValidator(validator: (OSCArgs) -> Boolean) =
+            copy(validator = validator)
 
-    @PublicApi
-    fun withValidator(validator: (OSCArgs) -> Boolean) =
-        copy(validator = validator)
+        @PublicApi
+        fun onError(onError: (OSCError) -> Unit) =
+            copy(onError = onError)
 
-    @PublicApi
-    fun onError(onError: (OSCError) -> Unit) =
-        copy(onError = onError)
+    }
 
 }
--- oscexchange/src/main/java/ac/at/fhstp/digitech/oscexchange/requests/SendRequest.kt
+++ oscexchange/src/main/java/ac/at/fhstp/digitech/oscexchange/requests/SendRequest.kt
@@ -21,39 +21,39 @@ data class SendRequest(
 
         @PublicApi
         fun new(address: OSCAddress) =
-            SendRequestBuilder(
+            Builder(
                 address, emptyArgsGenerator,
                 Request.noErrorHandling, noSuccessHandling
             )
 
     }
 
-}
+    data class Builder(
+        private val address: OSCAddress,
+        private val argsGenerator: (Unit) -> OSCArgs,
+        private val onError: (OSCError) -> Unit,
+        private val onSuccess: (Unit) -> Unit
+    ) : RequestBuilder<SendRequest> {
 
-data class SendRequestBuilder(
-    private val address: OSCAddress,
-    private val argsGenerator: (Unit) -> OSCArgs,
-    private val onError: (OSCError) -> Unit,
-    private val onSuccess: (Unit) -> Unit
-) : RequestBuilder<SendRequest> {
+        override fun build() =
+            SendRequest(address, argsGenerator, onError, onSuccess)
 
-    override fun build() =
-        SendRequest(address, argsGenerator, onError, onSuccess)
+        @PublicApi
+        fun withArgs(args: OSCArgs) =
+            withArgsGenerator(argsGenerator = { args })
 
-    @PublicApi
-    fun withArgs(args: OSCArgs) =
-        withArgsGenerator(argsGenerator = { args })
+        @PublicApi
+        fun withArgsGenerator(argsGenerator: (Unit) -> OSCArgs) =
+            copy(argsGenerator = argsGenerator)
 
-    @PublicApi
-    fun withArgsGenerator(argsGenerator: (Unit) -> OSCArgs) =
-        copy(argsGenerator = argsGenerator)
+        @PublicApi
+        fun onError(onError: (OSCError) -> Unit) =
+            copy(onError = onError)
 
-    @PublicApi
-    fun onError(onError: (OSCError) -> Unit) =
-        copy(onError = onError)
+        @PublicApi
+        fun onSuccess(onSuccess: (Unit) -> Unit) =
+            copy(onSuccess = onSuccess)
 
-    @PublicApi
-    fun onSuccess(onSuccess: (Unit) -> Unit) =
-        copy(onSuccess = onSuccess)
+    }
 
 }
