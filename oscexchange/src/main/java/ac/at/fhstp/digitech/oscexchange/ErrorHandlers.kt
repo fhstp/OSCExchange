c41988fa330a15bfdc54ada9da92d67779aee8ca
KingVanti <kingvanti650@gmail.com>
2021-06-16 10:55:17 +0200

    Add logic for handling errors by toasting them

--- oscexchange/src/main/java/ac/at/fhstp/digitech/oscexchange/ErrorHandlers.kt
+++ oscexchange/src/main/java/ac/at/fhstp/digitech/oscexchange/ErrorHandlers.kt
@@ -1,7 +1,9 @@
 package ac.at.fhstp.digitech.oscexchange
 
 import ac.at.fhstp.digitech.oscexchange.errors.OSCException
+import android.content.Context
 import android.util.Log
+import android.widget.Toast
 
 /**
  * Function that handles an OSCException
@@ -37,5 +39,19 @@ object ErrorHandlers {
     @PublicApi
     fun log(tag: String): ErrorHandler =
         { error -> Log.e(tag, error.message, error.cause) }
+
+    /**
+     * Creates an error handler that toasts the error
+     *
+     * @param context The context under which to create the toast
+     * @param length The length for how long to show the toast. This is a Toast.Duration
+     * @return The created error handler
+     */
+    @PublicApi
+    fun toast(
+        context: Context,
+        length: Int = Toast.LENGTH_SHORT
+    ): ErrorHandler =
+        { error -> Toast.makeText(context, error.message, length).show() }
 
 }
