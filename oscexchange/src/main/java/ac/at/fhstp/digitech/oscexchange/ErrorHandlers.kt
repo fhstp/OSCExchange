ecc23c50336013c257c668be40ca97e742f40204
KingVanti <kingvanti650@gmail.com>
2021-06-16 10:47:55 +0200

    Document ErrorHandlers

--- oscexchange/src/main/java/ac/at/fhstp/digitech/oscexchange/ErrorHandlers.kt
+++ oscexchange/src/main/java/ac/at/fhstp/digitech/oscexchange/ErrorHandlers.kt
@@ -3,17 +3,35 @@ package ac.at.fhstp.digitech.oscexchange
 import ac.at.fhstp.digitech.oscexchange.errors.OSCException
 import android.util.Log
 
+/**
+ * Function that handles an OSCException
+ */
 typealias ErrorHandler = (OSCException) -> Unit
 
+/**
+ * Contains some universal error-handlers
+ */
 object ErrorHandlers {
 
+    /**
+     * An error handler that does nothing
+     */
     val noErrorHandling: ErrorHandler =
         {}
 
+    /**
+     * An error handler that prints the error to System.out
+     */
     val print: ErrorHandler =
         { error -> println(error.message) }
 
 
+    /**
+     * Creates an error handler that logs the error
+     *
+     * @param tag The tag under which to log the message
+     * @return The created error handler
+     */
     fun log(tag: String): ErrorHandler =
         { error -> Log.e(tag, error.message, error.cause) }
 
