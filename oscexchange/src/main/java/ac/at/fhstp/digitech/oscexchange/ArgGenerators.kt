3a755f2af9ad69c5ee289826bd1ac8a5bbe40daf
KingVanti <kingvanti650@gmail.com>
2021-06-16 10:40:24 +0200

    Document ArgGenerators

--- oscexchange/src/main/java/ac/at/fhstp/digitech/oscexchange/ArgGenerators.kt
+++ oscexchange/src/main/java/ac/at/fhstp/digitech/oscexchange/ArgGenerators.kt
@@ -1,13 +1,28 @@
 package ac.at.fhstp.digitech.oscexchange
 
+/**
+ * A function that generates OSCArgs
+ */
 typealias ArgGenerator = () -> OSCArgs
 
+/**
+ * Contains some universal arg-generators
+ */
 object ArgGenerators {
 
+    /**
+     * Generates empty OSCArgs
+     */
     val empty: ArgGenerator =
         { OSCArgs.empty }
 
 
+    /**
+     * Always generates the given args
+     *
+     * @param args The args which this generator should generate
+     * @return The generator
+     */
     fun constant(args: OSCArgs): ArgGenerator =
         { args }
 
