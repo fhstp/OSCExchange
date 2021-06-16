987c882ff5c05ccc9b641735548741e1d3aeff72
KingVanti <kingvanti650@gmail.com>
2021-06-16 10:41:11 +0200

    Document ArgParser

--- oscexchange/src/main/java/ac/at/fhstp/digitech/oscexchange/ArgParsers.kt
+++ oscexchange/src/main/java/ac/at/fhstp/digitech/oscexchange/ArgParsers.kt
@@ -1,3 +1,7 @@
 package ac.at.fhstp.digitech.oscexchange
 
+/**
+ * Function that converts OSCArgs to some other type.
+ * The result might be null if no parsing was possible
+ */
 typealias ArgParser<TParsed> = (OSCArgs) -> TParsed?
