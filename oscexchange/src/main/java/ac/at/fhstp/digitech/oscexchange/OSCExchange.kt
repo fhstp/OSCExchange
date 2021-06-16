7ab268b11c82cb5eec1a58079a17693f45602503
KingVanti <kingvanti650@gmail.com>
2021-06-16 10:38:46 +0200

    Document OSCExchange

--- oscexchange/src/main/java/ac/at/fhstp/digitech/oscexchange/OSCExchange.kt
+++ oscexchange/src/main/java/ac/at/fhstp/digitech/oscexchange/OSCExchange.kt
@@ -4,12 +4,17 @@ import ac.at.fhstp.digitech.oscexchange.requests.Request
 import ac.at.fhstp.digitech.oscexchange.requests.RequestBuilder
 
 /**
- * Stores all information about an OSC exchange between two devices
+ * Stores all information about an OSC exchange.
+ * Must be made runnable before it can be executed
  */
+@PublicApi
 class OSCExchange private constructor(private val requests: Array<Request>) {
 
     companion object {
 
+        /**
+         * Starts building a new OSC exchange
+         */
         @PublicApi
         fun new() =
             Builder(listOf())
@@ -17,6 +22,12 @@ class OSCExchange private constructor(private val requests: Array<Request>) {
     }
 
 
+    /**
+     * Attempts to make this exchange runnable.
+     * For this ports must be opened, which could fail
+     * @param devicePair The pair between which the exchange should be run
+     * @return A result which could contain the runnable exchange
+     */
     @PublicApi
     fun tryMakeRunnable(devicePair: OSCDevicePair): Result<RunnableOSCExchange> {
         val inPortRes = devicePair.tryOpenInPort()
@@ -36,13 +47,24 @@ class OSCExchange private constructor(private val requests: Array<Request>) {
         )
     }
 
-
+    /**
+     * A builder class for OSCExchange
+     */
     data class Builder(private val requests: List<Request>) {
 
+        /**
+         * Adds a new request to this exchange.
+         * Requests are executed in the order they are added
+         * @param requestBuilder A builder for a request
+         */
         @PublicApi
         fun addRequest(requestBuilder: RequestBuilder<*>) =
             Builder(requests + requestBuilder.build())
 
+        /**
+         * Builds an OSCExchange from this builder
+         * @return The built OSCExchange
+         */
         @PublicApi
         fun build() =
             OSCExchange(requests.toTypedArray())
