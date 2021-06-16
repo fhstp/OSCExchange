bdffc6483a1f57f505212d6a8d3dee485fe36fa9
KingVanti <kingvanti650@gmail.com>
2021-06-16 10:59:19 +0200

    Document SuccessHandlers

--- oscexchange/src/main/java/ac/at/fhstp/digitech/oscexchange/SuccessHandler.kt
+++ oscexchange/src/main/java/ac/at/fhstp/digitech/oscexchange/SuccessHandler.kt
@@ -2,17 +2,39 @@ package ac.at.fhstp.digitech.oscexchange
 
 import android.util.Log
 
+/**
+ * Function that handles a successful request
+ */
 typealias SuccessHandler = () -> Unit
 
+/**
+ * Contains some common success-handlers
+ */
 object SuccessHandlers {
 
+    /**
+     * Does nothing
+     */
     val noSuccessHandling: SuccessHandler =
         {}
 
 
+    /**
+     * Prints a message to System.out
+     *
+     * @param message The message to print
+     * @return The created success handler
+     */
     fun print(message: String): SuccessHandler =
         { println(message) }
 
+    /**
+     * Prints a message to the log
+     *
+     * @param tag The tag under which to print the message
+     * @param message The message that should be printed
+     * @return The created success handler
+     */
     fun log(tag: String, message: String): SuccessHandler =
         { Log.i(tag, message) }
 
